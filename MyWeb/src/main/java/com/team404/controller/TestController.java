53f47c7180eee1d4866ab41a7420fc353413d09c
Your Name <you@example.com>
2020-08-06 15:38:30 +0900

    thrrtsjrtj

---
+++ MyWeb/src/main/java/com/team404/controller/TestController.java
@@ -0,0 +1,13 @@
+package com.team404.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("test")
+public class TestController {
+	@RequestMapping("/test")
+	public String freeRegist() {
+		return "test/test";
+	}
+}
